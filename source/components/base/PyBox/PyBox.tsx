e39761baff60a2336b202355c7a27dcf29573fad
KrzysztofZawisla <zawislakrzysztof.zk@gmail.com>
2022-05-17 22:10:35 +0200

    Added types

--- source/components/base/PyBox/PyBox.tsx
+++ source/components/base/PyBox/PyBox.tsx
@@ -1,11 +1,23 @@
-import { FC, PropsWithChildren } from "react";
+import {
+  DetailedHTMLProps,
+  FC,
+  HTMLAttributes,
+  PropsWithChildren,
+} from "react";
+import ReactElementProps from "~types/ReactElementProps/ReactElementProps";
 
-type PyBoxProperties = {};
+type PyBoxProperties = Omit<
+  ReactElementProps<
+    DetailedHTMLProps<HTMLAttributes<HTMLElement>, HTMLElement>
+  >,
+  "children"
+>;
 
-const PyBox: FC<PropsWithChildren<PyBoxProperties>> = ({
+const PyBox: FC<PyBoxProperties> = ({
   children,
+  ...rest
 }: PropsWithChildren<PyBoxProperties>) => {
-  return <py-box>{children}</py-box>;
+  return <py-box {...rest}>{children}</py-box>;
 };
 
 export default PyBox;
--- source/components/base/PyRepl/PyRepl.tsx
+++ source/components/base/PyRepl/PyRepl.tsx
@@ -1,6 +1,12 @@
-import { FC } from "react";
+import { DetailedHTMLProps, FC, HTMLAttributes } from "react";
+import ReactElementProps from "~types/ReactElementProps/ReactElementProps";
 
-export type PyReplProperties = {
+export type PyReplProperties = Omit<
+  ReactElementProps<
+    DetailedHTMLProps<HTMLAttributes<HTMLElement>, HTMLElement>
+  >,
+  "children"
+> & {
   autoGenerate?: boolean;
   children?: string;
 };
@@ -8,8 +14,13 @@ export type PyReplProperties = {
 const PyRepl: FC<PyReplProperties> = ({
   autoGenerate = false,
   children,
+  ...rest
 }: PyReplProperties) => {
-  return <py-repl auto-generate={autoGenerate}>{children}</py-repl>;
+  return (
+    <py-repl {...rest} auto-generate={autoGenerate}>
+      {children}
+    </py-repl>
+  );
 };
 
 export default PyRepl;
