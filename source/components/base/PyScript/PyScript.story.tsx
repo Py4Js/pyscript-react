b1df6100abee7e910e459abbc2d040b3bb3f5ea7
KrzysztofZawisla <zawislakrzysztof.zk@gmail.com>
2022-05-17 21:52:36 +0200

    Fixed format

--- source/components/base/PyScript/PyScript.story.tsx
+++ source/components/base/PyScript/PyScript.story.tsx
@@ -34,25 +34,22 @@ export const PyScriptExampleFibonacci: ComponentStory<typeof PyScript> =
 
 PyScriptExampleFibonacci.args = {
   children: `
-  class Fibonacci:
-    def __init__(self):
-        self.fibonacci_cache = {}
-    def sequence(self, n) -> int:
-        if n in self.fibonacci_cache:
-            return self.fibonacci_cache[n]
-        if n == 1:
-            return 1
-        elif n == 2:
-            return 1
-        elif n > 2:
-            self.fibonacci_cache[n] = self.sequence(n-1) + self.sequence(n-2)
-            return self.fibonacci_cache[n]
+class Fibonacci:
+  def __init__(self):
+      self.fibonacci_cache = {}
+  def sequence(self, n) -> int:
+      if n in self.fibonacci_cache:
+          return self.fibonacci_cache[n]
+      if n == 1:
+          return 1
+      elif n == 2:
+          return 1
+      elif n > 2:
+          self.fibonacci_cache[n] = self.sequence(n-1) + self.sequence(n-2)
+          return self.fibonacci_cache[n]
 
-
-  fibonacci = Fibonacci()
-  for x in range(100):
-    print(fibonacci.sequence(x))
-
-  
-  `,
+fibonacci = Fibonacci()
+for x in range(100):
+  print(fibonacci.sequence(x))
+`,
 };
