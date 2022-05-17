3a5ba0ac093e6b1ded640808385f8f74a03e378f
KrzysztofZawisla <zawislakrzysztof.zk@gmail.com>
2022-05-17 23:10:20 +0200

    Fixed types

--- source/components/base/PyButton/PyButton.tsx
+++ source/components/base/PyButton/PyButton.tsx
@@ -1,6 +1,12 @@
-import { FC } from "react";
+import { DetailedHTMLProps, FC, HTMLAttributes } from "react";
+import ReactElementProps from "~types/ReactElementProps/ReactElementProps";
 
-export type PyButtonProperties = {
+export type PyButtonProperties = Omit<
+  ReactElementProps<
+    DetailedHTMLProps<HTMLAttributes<HTMLElement>, HTMLElement>
+  >,
+  "children"
+> & {
   children: string;
   label: string;
 };
@@ -8,8 +14,13 @@ export type PyButtonProperties = {
 const PyButton: FC<PyButtonProperties> = ({
   children,
   label,
+  ...rest
 }: PyButtonProperties): JSX.Element => {
-  return <py-button label={label}>{children}</py-button>;
+  return (
+    <py-button {...rest} label={label}>
+      {children}
+    </py-button>
+  );
 };
 
 export default PyButton;
