3623b026acfc55d7d530cc810900389a430a6f89
ShootGan <nawelpalepka@gmail.com>
2022-05-17 23:10:32 +0200

    to futere refactor

--- source/components/folium/FoliumMap/utils/useGeoJson/useGeoJson.ts
+++ source/components/folium/FoliumMap/utils/useGeoJson/useGeoJson.ts
@@ -5,15 +5,18 @@ type UseGeoJsonsArguments = {
   geoJSON?: GeoJson[];
 };
 
+const getJson = async (url: string): Promise<object> => {
+  return await (await fetch(url)).json();
+};
+
 const useGeoJson = ({ mapName, geoJSON }: UseGeoJsonsArguments) => {
   return geoJSON && mapName && Array.isArray(geoJSON)
     ? geoJSON
         .map(({ jsonPath, name }) => {
+          const data = getJson(jsonPath);
           return `
-
 from json import loads;
-
-data = loads()
+data = loads(${data})
 folium.GeoJson(data${
             name ? `, name="${name}"` : `,name="geojson"`
           }).add_to(${mapName})`;
