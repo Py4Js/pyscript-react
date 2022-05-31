6750bb2a753a70d90fdbe35a669f54c7a210d257
KrzysztofZawisla <zawislakrzysztof.zk@gmail.com>
2022-05-31 20:29:51 +0200

    Added support for children as object

--- source/components/base/PyEnv/PyEnv.tsx
+++ source/components/base/PyEnv/PyEnv.tsx
@@ -1,6 +1,24 @@
 import propTypes from "prop-types";
-import { DetailedHTMLProps, FC, HTMLAttributes, useMemo } from "react";
+import {
+  DetailedHTMLProps,
+  FC,
+  HTMLAttributes,
+  useMemo,
+  WeakValidationMap,
+} from "react";
 import ReactElementProps from "~types/ReactElementProps/ReactElementProps";
+
+type ComplexChildren = ComplexChildrenWithPaths | ComplexChildrenWithoutPaths;
+
+type ComplexChildrenWithPaths = {
+  paths: string[];
+  items?: string[];
+};
+
+type ComplexChildrenWithoutPaths = {
+  paths?: string[];
+  items: string[];
+};
 
 export type PyEnvProperties = Omit<
   ReactElementProps<
@@ -8,7 +26,7 @@ export type PyEnvProperties = Omit<
   >,
   "children"
 > & {
-  children: string | string[];
+  children: string | string[] | ComplexChildren;
 };
 
 const PyEnv: FC<PyEnvProperties> = ({
@@ -16,19 +34,43 @@ const PyEnv: FC<PyEnvProperties> = ({
   ...rest
 }: PyEnvProperties): JSX.Element => {
   const fixedChildren: string = useMemo((): string => {
-    return Array.isArray(children)
-      ? children
-          .map((element: string): string => {
+    const { paths, items }: ComplexChildren = Object(children);
+    if (paths || items) {
+      const fixedItems: string =
+        items
+          ?.map((element: string): string => {
             return `- ${element}`;
           })
-          .join("\n")
-      : children;
+          .join("\n") || "";
+      const fixedPaths: string =
+        paths
+          ?.map((element: string): string => {
+            return `\t- ${element}`;
+          })
+          .join("\n") || "";
+      return `${fixedItems}${fixedPaths ? `\n- paths:\n${fixedPaths}` : ""}`;
+    } else {
+      return Array.isArray(children)
+        ? children
+            .map((element: string): string => {
+              return `- ${element}`;
+            })
+            .join("\n")
+        : (children as string);
+    }
   }, [children]);
   return <py-env {...rest}>{fixedChildren}</py-env>;
 };
 
 PyEnv.propTypes = {
-  children: propTypes.oneOfType([propTypes.string, propTypes.array]).isRequired,
-};
+  children: propTypes.oneOfType([
+    propTypes.string,
+    propTypes.array,
+    propTypes.exact({
+      items: propTypes.arrayOf(propTypes.string),
+      paths: propTypes.arrayOf(propTypes.string),
+    }),
+  ]).isRequired,
+} as WeakValidationMap<PyEnvProperties>;
 
 export default PyEnv;
