abf579b4557d0a940b8cd83d25648fec0416e09d
KrzysztofZawisla <zawislakrzysztof.zk@gmail.com>
2022-06-01 00:03:47 +0200

    Added build command as TypeScript file

---
+++ scripts/runner/commands/build/build.ts
@@ -0,0 +1,11 @@
+import { platform } from "os";
+import { $ } from "zx";
+
+(async (): Promise<void> => {
+  const os: NodeJS.Platform = platform();
+  if (os === "win32") {
+    $.shell = "cmd";
+    $.prefix = "";
+  }
+  await $`cross-env rimraf destination && cross-env tsc && cross-env TS_NODE_PROJECT=tsconfig.node.json webpack`;
+})();
