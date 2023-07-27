56dd44db35d963bda144d86f8b6c140cd58d7b7a
KrzysztofZawisla <zawislakrzysztof.zk@gmail.com>
2023-07-27 19:24:38 +0200

    added forwardRef to py-splashscreen

--- source/library/components/py-terminal/py-terminal.tsx
+++ source/library/components/py-terminal/py-terminal.tsx
@@ -1,12 +1,15 @@
 import propTypes from "prop-types";
-import { forwardRef } from "react";
+import { ForwardedRef, forwardRef } from "react";
+
 import type { PyTerminalProperties, PyTerminalTag } from "./py-terminal.types";
 
 const PyTerminal: PyTerminalTag = forwardRef(
   <OptionalProperties extends object>(
     properties: PyTerminalProperties<OptionalProperties>,
+    reference: ForwardedRef<HTMLElement> | undefined,
+    // eslint-disable-next-line max-params
   ): JSX.Element => {
-    return <py-terminal {...properties} />;
+    return <py-terminal ref={reference} {...properties} />;
   },
 ) as PyTerminalTag;
 
